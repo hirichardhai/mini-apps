016bd1131429340f1a91b36505990f8a426304ae
Richard Hai <hirichardhai@gmail.com>
2018-02-12 14:46:21 -0800

    added ability to mark each cell in table with x

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,32 @@
-console.log('hello')
+app = {
+    init: () => {
+        app.controller.toggle()
+    },
+
+    model: {
+        types: ['X', 'O']
+    },
+
+    view: {
+
+    },
+
+    controller: {
+        toggle: function(cell) {
+            if (cell.innerHTML)
+            cell.innerHTML = 'x';
+        }
+    
+    }
+}
+
+var element = document.querySelectorAll("td")
+
+element.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        console.log(cell.id)
+        app.controller.toggle(cell)
+})})
+
+
+// console.log(element.length)
