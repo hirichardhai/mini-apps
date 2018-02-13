df20b31b87876faaf9f8d01eba53890e28f57822
Richard Hai <hirichardhai@gmail.com>
2018-02-12 23:32:34 -0800

    attempting to server app.js

--- challenge_2/myapp/server.js
+++ challenge_2/myapp/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 
 app.get('/', function(req, res){
-    res.sendFile('index.html', { root: '../client'})});
+    res.sendFile('index.html', { root: '../client'});
+});
+
+// app.get('/', function(req, res){
+//     res.sendFile('app.js', {root: '../client'})
+// });
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
