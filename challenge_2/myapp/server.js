56f6e287c65d65c2e3dde21a830fef0bb65e6cfd
Richard Hai <hirichardhai@gmail.com>
2018-02-13 13:58:05 -0800

    added recursion for parsing

--- challenge_2/myapp/server.js
+++ challenge_2/myapp/server.js
@@ -1,30 +1,38 @@
-// var convertJSON = (data) => {
-//     var string = '';
-//     var keys = [];
-//     for (var key in data) {
-//         if (key !== 'children') {
-//             string += `${key},`
-//             keys.push(key);
-//         }
-//     }
+var convertJSON = (data) => {
+    var string = '';
+    var keys = [];
+    for (var key in data) {
+        if (key !== 'children') {
+            string += `${key},`
+            keys.push(key);
+        }
+    }
+    string = string.substring(0, string.length -1);
+    string += '\n'
 
-//     var extractValues = (obj) => {
-//         if (!Array.isArray(obj)) {
-//             for (var i = 0; i < keys.length; i++) {
-//                 string += `${obj[keys[i]]},`
-//             }
-//         }
-//         if (Array.isArray(obj)) {
+    var extractValues = (obj) => {
+        if (!Array.isArray(obj)) {
+            for (var i = 0; i < keys.length; i++) {
+                string += `${obj[keys[i]]},`
+            }
+            string = string.substring(0, string.length -1);
+            string += '\n'
+        }
+        if (Array.isArray(obj)) {
+            for (var i = 0; i < keys.length; i++) {
+                string += `${obj[keys[i]]}`
+            }
+            string = string.substring(0, string.length -1);
+            string += '\n'
+        }
+        for (var i = 0; i < obj.children.length; i++) {
+            extractValues(obj.children[i]);
+        }
+    }
 
-//         }
-//         for (var i = 0; i < data.children.length; i++) {
-            
-//         }
-//     }
-
-//     extractValues(data);
-//     return string;
-// }
+    extractValues(data);
+    return string;
+}
 
 const express = require('express');
 const app = express();
@@ -39,7 +47,7 @@ app.use(express.static('../client'));
 app.post('/', (req, res) => {
     console.log(req.body);
     res.send(req.body);
-    // res.send(convertJSON(req.body));
+    res.send(convertJSON(req.body));
 });
 
 app.listen(3000);
