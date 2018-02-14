d1823980f04f1015023dde3fc9ff7e9f63617644
Richard Hai <hirichardhai@gmail.com>
2018-02-13 23:25:46 -0800

    setup react for challenge 3

--- challenge_2/myapp/server.js
+++ challenge_2/myapp/server.js
@@ -1,44 +1,45 @@
-var convertJSON = (data) => {
-    var string = '';
-    var keys = [];
-    for (var key in data) {
-        if (key !== 'children') {
-            string += `${key},`
-            keys.push(key);
-        }
-    }
-    string = string.substring(0, string.length -1);
-    string += '\n'
+// var convertJSON = (data) => {
+//     var string = '';
+//     var keys = [];
+//     for (var key in data) {
+//         if (key !== 'children') {
+//             string += `${key},`
+//             keys.push(key);
+//         }
+//     }
+//     string = string.substring(0, string.length -1);
+//     string += '\n'
 
-    var extractValues = (obj) => {
-        if (!Array.isArray(obj)) {
-            for (var i = 0; i < keys.length; i++) {
-                string += `${obj[keys[i]]},`
-            }
-            string = string.substring(0, string.length -1);
-            string += '\n'
-        }
-        if (Array.isArray(obj)) {
-            for (var i = 0; i < keys.length; i++) {
-                string += `${obj[keys[i]]}`
-            }
-            string = string.substring(0, string.length -1);
-            string += '\n'
-        }
-        for (var i = 0; i < obj.children.length; i++) {
-            extractValues(obj.children[i]);
-        }
-    }
+//     var extractValues = (obj) => {
+//         if (!Array.isArray(obj)) {
+//             for (var i = 0; i < keys.length; i++) {
+//                 string += `${obj[keys[i]]},`
+//             }
+//             string = string.substring(0, string.length -1);
+//             string += '\n'
+//         }
+//         if (Array.isArray(obj)) {
+//             for (var i = 0; i < keys.length; i++) {
+//                 string += `${obj[keys[i]]}`
+//             }
+//             string = string.substring(0, string.length -1);
+//             string += '\n'
+//         }
+//         for (var i = 0; i < obj.children.length; i++) {
+//             extractValues(obj.children[i]);
+//         }
+//     }
 
-    extractValues(data);
-    return string;
-}
+//     extractValues(data);
+//     return string;
+// }
 
 const express = require('express');
 const app = express();
 const path = require('path');
 var cors = require('cors');
 const bodyParser = require('body-parser');
+
 app.use(cors());
 app.use(bodyParser.json());
 
